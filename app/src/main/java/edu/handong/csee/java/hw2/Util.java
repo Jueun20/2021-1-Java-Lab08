c12dfdcc05a6437e8104f7f8474f9dfa5ed742a1
Jueun20 <22000350@handong.edu>
2021-05-06 16:39:33 +0900

    Util.java

---
+++ app/src/main/java/edu/handong/csee/java/hw2/Util.java
@@ -0,0 +1,12 @@
+package edu.handong.csee.java.hw2;
+
+/**
+ * This is class for getting upperingcaseString.
+ */
+public class Util {
+
+    static String getUppercaseString(String stringValue) {
+        return stringValue.toUpperCase();
+    }
+    
+}
